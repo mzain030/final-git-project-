af5a226cc2561be33137b964f65cd6a4a9c66afe
junaidarshad1122 <jj8286014@gmail.com>
2026-01-31 11:54:05 -0800

    input button

--- src/components/Junaid.jsx
+++ src/components/Junaid.jsx
@@ -5,8 +5,13 @@ const junaid = () => {
     <div>
       <h1>junaidkhan6877</h1>
 <h2>my self muhammad junaid</h2>
+
+<input type="text" placeholder='search option ' />
+<button >Add Calborater </button>
+
     
     </div>
+    
   )
 }
 
