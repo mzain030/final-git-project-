acee56f8abb3151c0befaffd4afb36a96c41d752
abdullah <you@example.com>
2026-01-31 11:46:47 +0500

    True

---
+++ src/components/AS/Counting.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Counting = () => {
+  return (
+    <div>
+     <h1>123</h1>
+     <h2>aadhded</h2>
+     <h3>endcjsvdf</h3>
+     <h4>ncvjsfvjsd</h4>
+    </div>
+  )
+}
+
+export default Counting
