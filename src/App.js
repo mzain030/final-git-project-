cc88869dd5f142f7cb31ec0fc77d5beaedaefb9a
junaidarshad1122 <jj8286014@gmail.com>
2026-01-31 11:48:18 -0800

    cd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import LearnUseState from './components/LearnUseState';
+import junaid from './components/junaid';
 
 function App() {
   return (
     <div className="App">
       <LearnUseState/>
+      <Junaid/>
     </div>
   );
 }
---
+++ src/components/Junaid.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const junaid = () => {
+  return (
+    <div>
+      <h1>junaidkhan6877</h1>
+<h2>my self muhammad junaid</h2>
+    
+    </div>
+  )
+}
+
+export default junaid
