09bbc9c2c08e521d6756579e340ee9fcff3819f2
abdullah <you@example.com>
2026-01-31 11:54:29 +0500

    excel

--- src/components/AS/Counting.jsx
+++ src/components/AS/Counting.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Counting = () => {
+  const [color, setColor] = useState('pink');
+ 
   return (
     <div>
      <h1>123</h1>
@@ -8,6 +10,9 @@ const Counting = () => {
      <h3>endcjsvdf</h3>
      <h3>Abdullah</h3>
      <h4>ncvjsfvjsd</h4>
+      <button type='button' onClick={() => setColor('Yellow')}>Click Yellow</button>
+            <button type='button' onClick={() => setColor('Orange')}>Click Orange</button>
+            <button type='button' onClick={() => setColor('Brown')}>Click Brown</button>
     </div>
   )
 }
