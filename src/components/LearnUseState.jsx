0b0e7ce6a3b9cfa8aa44baa0049c118e6d77da99
mzain030 <mzainnaseer786@gmail.com>
2026-01-31 10:37:43 +0500

    testingapp crud

---
+++ src/components/LearnUseState.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const LearnUseState = () => {
+    const [color, setColor] = useState('Red');
+
+    return (
+        <div>
+            <h1>UseState</h1>
+            <h1>My faviourt color is {color}</h1>
+            <button type='button' onClick={() => setColor('Blue')}>Click me</button>
+            <button type='button' onClick={() => setColor('Green')}>Click Green</button>
+            <button type='button' onClick={() => setColor('Yellow')}>Click Yellow</button>
+            <button type='button' onClick={() => setColor('Orange')}>Click Orange</button>
+            <button type='button' onClick={() => setColor('Brown')}>Click Brown</button>
+        </div>
+    )
+}
+
+export default LearnUseState;
